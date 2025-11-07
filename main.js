af160b1065a2e19e7f774906faa62617aeaae103
axel3023 <133275903+axel3023@users.noreply.github.com>
2025-11-07 14:42:22 -0600

    Update service worker registration path

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/cocteles-pwa/sw.js')
+        navigator.serviceWorker.register('/sw.js')
             .then(reg => console.log('Registro SW exitoso: ', reg))
             .catch(err => console.error('Error de registro SW: ', err));
     });
